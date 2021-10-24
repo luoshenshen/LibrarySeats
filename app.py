24db3e698c59144d5bb291937260f2f21f2467f5
luoshenshen <luoshenshen@buaa.edu.cn>
2021-10-24 15:02:35 +0800

    发布二进制可执行软件包

--- app.py
+++ app.py
@@ -4,22 +4,54 @@
 @ user: 罗申申
 @ email: luoshenshen@buaa.edu.cn
 @ tool: PyCharm
-@ time: 2021/5/23 18:39
+@ time: 2021/6/14 10:27
 '''
-import re
-import function
-import browser_tools
+#-*- coding:utf-8 -*-
 import time
+import tornado.web
+import tornado.ioloop
+import api
+from threading import Thread
+import browser_tools
+import requests
 
-def run(cookie,floor,flag):
-    result = function.session_get(browser_tools.index_url, browser_tools.get_index_header(cookie,time.time()))
-    if '来选座' not in result.text:
-        return "进入来选座系统失败，Cookie不正确"
-    result = function.session_get(browser_tools.center_url, browser_tools.get_center_header(cookie,time.time()))
-    nick = re.findall('<div class="nick">(.*)</div>', result.text)[0]
-    result = function.session_get(browser_tools.prereserve_url, browser_tools.get_prereserve_header(cookie,time.time()))
-    if nick in result.text or "预约明天的座位" in result.text:
-        return function.fecth(cookie,floor,flag)
-    else:
-        return "进入来选座系统失败，Cookie不正确"
+cookies = []
 
+requests.packages.urllib3.disable_warnings()
+requests_with_session = requests.Session()
+
+def keep_alive():
+
+    while 1:
+        for i in cookies:
+            print(requests_with_session.get(url='https://wechat.laixuanzuo.com/index.php/reserve/index.html',
+                                  headers=browser_tools.get_index_header(i,time.time()), allow_redirects=False, verify=False).text)
+        time.sleep(300)
+
+class IndexHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        self.render("seat.html")
+
+    def post(self):
+        cookie = self.get_body_argument('cookie', '')
+        floor = self.get_body_argument('floor', '')
+        flag = self.get_body_argument('flag', '')
+        if flag == '':
+            goal = True
+        else:
+            goal = False
+        result = "未订阅或不符合选座要求"
+        if cookie == '' or floor == '':
+            return result
+        result = api.run(cookie, floor,goal)
+        print(result)
+        cookies.append(cookie)
+        Thread(target=keep_alive).start()
+        self.write(result)
+
+if __name__ == '__main__':
+
+    app = tornado.web.Application([(r'/app/seat',IndexHandler)])
+    app.listen(port=8081)
+    tornado.ioloop.IOLoop.current().start()
