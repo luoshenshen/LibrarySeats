8e6176f7f9e9176fa0ca3e51c82f96c4b32204f2
学会了你最爱的开朗 <luoshenshen.dev@gmail.com>
2021-11-25 20:24:49 +0800

    解决百度网络验证码timeout问题

--- function.py
+++ function.py
@@ -251,7 +251,7 @@ def fecth(cookie,floor,k,flag,nick):
                 print("需要再等" + str((19 - int(hour))) + "小时")
             hour, min, sec = timer.times()
         # 时间控制：小时：eg 阁下学校开始抢座时间 如：19：50 ，控制分钟 50
-        while int(min) < int(1):
+        while int(min) < int(50):
             time.sleep(0.2)
             alive += 1
             if alive == 360:
