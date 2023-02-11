a278c26f95e268155bd822d57e9fbddf60fed8b1
luoshenshen <luoshenshen@buaa.edu.cn>
2023-02-12 01:53:00 +0800

    新增核心功能

---
+++ GoToLibrary/core.py
@@ -0,0 +1,9 @@
+''' 
+* - * - * - * - * - * - * - * - * - * - * - * - * - *
+@project: LibrarySeats
+@file: core.py
+@ide: PyCharm
+@author: ShenshenLuo
+@time: 2023/2/12 1:52:33
+* - * - * - * - * - * - * - * - * - * - * - * - * - *
+'''
